afac0529be3758dc632d8bcd5b943b1201315bdf
macmini62 <victorkangacha@gmail.com>
2025-01-14 00:53:30 +0300

    updated the auth setup options

--- frontend/src/app/auth/setup/layout.tsx
+++ frontend/src/app/auth/setup/layout.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import Link from "next/link";
 import React from "react";
 
 const setupLayout = ({
@@ -9,8 +10,19 @@ const setupLayout = ({
 }) => {
 
   return (
-    <div className="self-start mt-10 w-1/3 shadow-lg shadow-gray-300 rounded-md p-4">
-      {children}
+    <div className="w-full h-full flex flex-col">
+      <header className="self-start w-full h-32 flex items-center px-4">
+        <img src="/logo/logo-white.png" alt="" className="w-48 h-14" />
+      </header>
+      <hr className="w-full border-black border-2" />
+      <div className="w-full h-[calc(100%-84px)] flex justify-center pt-8">
+        {children}
+      </div>
+      <hr className="w-full border-black border-2" />
+      <div className="flex items-center justify-between w-full h-20 px-8">
+        <button className="w-32 h-12 bg-black rounded-full active:bg-white active:border border-black active:text-black text-white">Back</button>
+        <button className="w-32 h-12 bg-black rounded-full active:bg-white active:border border-black active:text-black text-white">Continue</button>
+      </div>
     </div>
   )
 }
