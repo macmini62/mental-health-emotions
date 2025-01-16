74b1be67f44997705e2bdb03f621ed09d73902e9
macmini62 <victorkangacha@gmail.com>
2025-01-16 22:21:35 +0300

    updated the platform's footer

---
+++ frontend/src/app/components/footerOptions/footer.tsx
@@ -0,0 +1,26 @@
+import Link from "next/link";
+
+const Footer = () => {
+  const options = [
+    "help",
+    "about",
+    "careers",
+    "privacy",
+    "terms"
+  ]
+
+  return (
+    <div className="flex flex-col items-center text-sm">
+      <div className="flex gap-3 my-4">
+        {
+          options.map((o: string, i: number) => (
+            <Link href={`/emotions/${o}`} key={i} className="hover:underline hover:text-black capitalize">{o}</Link>
+          ))
+        }
+      </div>
+      <p className="text-black">emotions&trade; &copy; {(1970+(Date.now()/(1000*60*60*24*366))).toFixed(0)}</p>
+    </div>
+  )
+}
+
+export default Footer;
