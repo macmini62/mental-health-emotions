24262f7de0a6e821313012951e105414ec70645f
macmini62 <victorkangacha@gmail.com>
2025-01-27 23:32:07 +0300

    updated the article creation section and resolved the errors

--- frontend/src/app/create/art/page.tsx
+++ frontend/src/app/create/art/page.tsx
@@ -3,65 +3,13 @@
 import Link from "next/link";
 import { SlOptions } from "react-icons/sl";
 import * as React from "react";
-import { styled } from "@mui/material/styles";
-import Button from "@mui/material/Button";
-import { FiUploadCloud } from "react-icons/fi";
-import { MdDeleteOutline } from "react-icons/md";
-import InsertOptions from "@/app/components/createComponents/insertOptions";
 import ImageSection from "@/app/components/createComponents/imageSection";
 import ContentSection from "@/app/components/createComponents/contentSection";
 
-const VisuallyHiddenInput = styled("input")({
-  clip: "rect(0 0 0 0)",
-  clipPath: "inset(50%)",
-  height: 1,
-  overflow: "hidden",
-  position: "absolute",
-  bottom: 0,
-  left: 0,
-  whiteSpace: "nowrap",
-  width: 1,
-});
-
-const buttonStyle = {
-  backgroundColor: "transparent",
-  color: "black",
-  boxShadow: "none",
-  border: "2px solid black",
-  borderRadius: "10px"
-};
 
 const Page = () => {
 
-  const [thumbnail, setThumbnail] = React.useState<string>();
-  const handleThumbnailUpload = (imgData: FileList | null) => {
-    console.log(imgData);
-    setThumbnail(() => {
-      const selImg = imgData?.item(0);
-      if(selImg !== null){
-        if(selImg?.type.split("/")[0] === "image"){
-          const img = URL.createObjectURL(selImg);
-          return img.toString();
-        }else{
-          console.log("File uploaded must be an image!!");
-        }
-      }
-    });
-  }
-  // console.log(thumbnail)
-
-  const [caption, setCaption] = React.useState<string>("");
-  const handleCaption = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const target = event.target as HTMLInputElement;
-    setCaption(target.value);
-  }
-
-  const [content, setContent] = React.useState<React.ReactNode[]>([
-    // <InsertOptions
-    //   uploadImage={(imgData: FileList | null) => handleImageUpload(imgData)}
-    //   insertOptions={handleInsertOptions}
-    // />
-  ]);
+  const [content, setContent] = React.useState<Array<React.JSX.Element>>([]);
 
   const handleImageUpload = (imgData: FileList|null) => {
     // console.log(imgData);
@@ -69,35 +17,33 @@ const Page = () => {
     if(selImg !== null){
       if(selImg?.type.split("/")[0] === "image"){
         const img = URL.createObjectURL(selImg);
-        setContent((c: React.ReactNode[]) => {
-          c.unshift(
+        setContent((c: Array<React.JSX.Element>) => {
+          return[
+            ...c,
             <ImageSection
+              key={c.length}
               image={img}
+              deleteImage={(key: number) => handleDeleteContent(key)}
             />
-          );
-          return[...c];
-        });
+          ];
+        })
       }else{
         console.log("File uploaded must be an image!!");
       }
     }
   }
+  console.log(content);
 
   const handleInsertParagraph = () => {
-
+    
+  }
+  
+  const handleDeleteContent = (key: number) => {
+    console.log("Key:", key);
   }
 
-  const [contentSection, setContentSection] = React.useState<Array<React.ReactNode>>([
-    <ContentSection
-      handleInsertParagraph={() => handleInsertParagraph()}
-      handleUploadImage={(imgData: FileList|null) => handleImageUpload(imgData)}
-      content={undefined}
-    />
-  ]);
+  const [contentSection, setContentSection] = React.useState<Array<React.ReactNode>>([]);
 
-  const handleContentSection = () => {
-
-  }
   
   return (
     <div className="w-1/2 p-2 relative">
@@ -117,64 +63,13 @@ const Page = () => {
         {/* titles */}
         <input type="text" className="text-7xl text-black outline-none px-2 font-semibold placeholder:font-normal" placeholder="TITLE"/>
         <input type="text" className="text-4xl text-black outline-none px-6" placeholder="SUB-TITLE"/>
-        {/* thumbnail */}
-        <div className="flex flex-col items-center gap-2 py-2 px-8 my-4">
-          {
-            !thumbnail &&
-            <Button
-              component="label"
-              role={undefined}
-              variant="contained"
-              tabIndex={-1}
-              startIcon={<FiUploadCloud />}
-              style={{...buttonStyle, visibility: "visible"}}
-            >
-              upload thumbnail
-              <VisuallyHiddenInput
-                type="file"
-                onChange={(event) => handleThumbnailUpload(event.target.files)}
-              />
-            </Button>
-          }
-          <div className="my-4 flex flex-col items-center relative group">
-            { thumbnail && <img src={thumbnail} alt="" className="w-[450px] h-[450px] rounded-md" /> }
-            <div className="flex items-center justify-center absolute z-10 w-full h-full invisible group-hover:visible bg-gray-50 opacity-80 rounded-md">
-              <Button
-                component="label"
-                role={undefined}
-                variant="contained"
-                tabIndex={-1}
-                startIcon={<MdDeleteOutline />}
-                style={buttonStyle}
-                onClick={() => setThumbnail(undefined)}
-              >
-                delete thumbnail
-              </Button>
-            </div>
-          </div>
-          {
-            thumbnail &&
-            <div className="flex flex-col items-center gap-4 w-full">
-              <input
-                name="caption"
-                type="text"
-                value={caption}
-                placeholder="Caption"
-                className="text-sm text-gray-500 w-fit py-1 px-2 border-b-2 border-black outline-none max-w-fit"
-                onChange={(event) => handleCaption(event)}
-              />
-            </div>
-          }
-        </div>
         {/* content */}
         <div className="">
-          {
-            contentSection.map((cs: React.ReactNode, i: number) => (
-              <React.Fragment key={i}>
-                {cs}
-              </React.Fragment>
-            ))
-          }
+          <ContentSection
+            handleInsertParagraph={handleInsertParagraph}
+            handleUploadImage={handleImageUpload}
+            content={content}
+          />
         </div>
       </div>
     </div>
