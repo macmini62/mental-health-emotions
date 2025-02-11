9207536bd0ea3e29d63350a81a1de48fd7ca2cca
macmini62 <victorkangacha@gmail.com>
2025-02-11 08:20:41 +0300

    Updated the users schema

--- backend/src/Users/professionals/schema/professional.schema.ts
+++ backend/src/Users/professionals/schema/professional.schema.ts
@@ -24,21 +24,43 @@ export class Professional {
   profession: string;
 
   @Prop({ required: true })
-  permissions: boolean;
+  permissions: true;
+  
+  @Prop({ required: true })
+  joiningDate: string;
+
+  @Prop()
+  lastActive: string;
 
   @Prop({
     type: {
       url: { type: String, required: true },
       height: { type: Number, required: true },
       width: { type: Number, required: true },
-    },
-    required: true,
+    }
   })
   image: {
     url: string;
     height: number;
     width: number;
   };
+  
+  @Prop({
+    type: {
+      topics: { type: String },
+      bookmarks: { 
+        articles: { type: String },
+        videos: { type: String }
+      },
+    }
+  })
+  contents: {
+    topics: string[];
+    bookmarks: {
+      articles: string[];
+      videos: string[];
+    }
+  }
 }
 
 export const ProfessionalSchema = SchemaFactory.createForClass(Professional);
--- backend/src/Users/seekers/schema/seeker.schema.ts
+++ backend/src/Users/seekers/schema/seeker.schema.ts
@@ -18,24 +18,46 @@ export class Seeker {
   email: string;
 
   @Prop({ required: true })
-  password: string;
+  password: string
 
   @Prop({ required: true })
-  permissions: boolean;
+  permissions: false;
+  
+  @Prop({ required: true })
+  joiningDate: string;
+
+  @Prop()
+  lastActive: string;
 
   @Prop({
     type: {
       url: { type: String, required: true },
       width: { type: Number, required: true },
       height: { type: Number, required: true },
-    },
-    required: true,
+    }
   })
   image: {
     url: string;
     width: number;
     height: number;
   };
+
+  @Prop({
+    type: {
+      topics: { type: String },
+      bookmarks: { 
+        articles: { type: String },
+        videos: { type: String }
+      },
+    }
+  })
+  contents: {
+    topics: string[];
+    bookmarks: {
+      articles: string[];
+      videos: string[];
+    }
+  }
 }
 
 export const SeekerSchema = SchemaFactory.createForClass(Seeker);
