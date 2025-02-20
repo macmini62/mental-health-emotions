516169b8e9d57f0a6065da2f7d7dec5012fd3e8f
macmini62 <victorkangacha@gmail.com>
2025-02-20 10:56:58 +0300

    feat: add complete registration endpoint to AuthController; inject UsersService for user management

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Post, Body, HttpCode, HttpStatus, Res, Get, Req, UseGuards } from "@nestjs/common";
+import { Controller, Post, Body, HttpCode, HttpStatus, Res, Get, Req, UseGuards, Param } from "@nestjs/common";
 import { AuthService } from "./auth.service";
 import { user } from "src/users/interface/user.interface";
 import { SkipAuth } from "src/decorators/auth.decorator";
 import { Response } from "express";
 import { AuthGuard } from "src/guards/auth.guard";
+import { UsersService } from "src/users/users.service";
 
 @Controller("auth")
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly userService: UsersService
+  ) {}
 
   @SkipAuth()
   @Post("signup")
@@ -17,6 +21,19 @@ export class AuthController {
       res.status(201).send(accessToken);
     }
     res.status(500).send("User email already in user!!");
+  }
+
+  @SkipAuth()
+  @Post("signup/completeRegistration/:userId")
+  async completeSignUp(
+    @Param("userId") userId: string,
+    @Body() data: {
+      role: string,
+      title: string,
+      topics: string[]
+    }
+  ){
+    return await this.userService.addUserProfessional(userId, data);
   }
 
   @SkipAuth()
